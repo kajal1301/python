010ab5ea996b2907c58337d448e0065e85bf3d7d
Arpit tiwari <38579437+arpitiiitv@users.noreply.github.com>
2019-10-02 09:25:16 +0530

    Fixed bug
    You have taken discriminant as b**2 - 2*a*c instead of b**2 - 4*a*c

--- quadraticEqn.py
+++ quadraticEqn.py
@@ -1,21 +1,21 @@
 #python program to solve quadratic equation
 from math import sqrt
 
-print("Quadratic Equation: a*x1 + b*x2 + c")
-a= float(input("a:"))
-b= float(input("b:"))
-c= float(input("C:"))
-r= b**2 + 2*a*c
+print("Quadratic Equation: a*x^2 + b*x + c=0")
+a= float(input("Coeff. of x^2   a: "))
+b= float(input("Coeff. of x     b: "))
+c= float(input("constant term   C: "))
+r= b**2 - 4*a*c
 if r>0:
     number_of_roots= 2
     x1= (-b + sqrt(r))/(2*a)
     x2= (-b - sqrt(r))/(2*a)
     print("the Equation has two roots: ",(x1,x2))
-if r==0:
+elif r==0:
     number_of_roots= 1
     x= -b/(2*a)
-    print("the Equation has one root:", x)
+    print("the Equation has repeated root: ", x)
 else:
     number_of_roots= 0
-    print("no roots as discriminat<0")
+    print("no roots as discriminant < 0")
     exit()
