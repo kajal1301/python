200af427daba661ce04638dc9ef7fc8c73d459aa
Arpit tiwari <38579437+arpitiiitv@users.noreply.github.com>
2019-10-03 15:27:24 +0530

    added intersection method

--- set.py
+++ set.py
@@ -21,8 +21,6 @@ x= {"set","tuple","lists"}
 #to delete the set completely
 del x
 
-
-
 #join two sets
 x1= {"a","b","c"}
 x2= {1,2,3}
@@ -31,3 +29,8 @@ print(x3)
 #update the set
 x1.update(x2)
 print(x1)
+
+#intersection 
+a={1,2,3,4,5}
+b={4,5,6,7,8}
+print(a.intersection(b))
